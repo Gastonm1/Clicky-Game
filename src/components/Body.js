185369f0ca6c80c6058a092f059f600fb534fe8b
Gastonm1 <gaston.marques@gmail.com>
2019-04-30 16:30:27 -0500

    divs have images

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 // import images from '../images';
 
+const selected = [];
+
 const images = {
-  mcDonalds: require("../images/McDs.jpeg")
+  mcDonalds: require("../images/McDs.jpeg"),
+  blaze: require("../images/Blaze.jpg"),
+  chik: require("../images/chic-fil-a.png"),
+  chipotle: require("../images/Chipotle.png"),
+  fiveGuys: require("../images/Five Guys.jpg"),
+  freshii: require("../images/freshii.jpg"),
+  greekKitchen: require("../images/Greek Kitchen.jpg"),
+  panera: require("../images/Panera Bread.png"),
+  penn: require("../images/Penn Station.jpeg"),
+  popeyes: require("../images/Popeyes.jpg"),
+  portillos: require("../images/portillos.gif"),
+  wendys: require("../images/wendys.jpg"),
 }
+
+
 
 export default class Body extends Component {
   render() {
@@ -11,17 +26,17 @@ export default class Body extends Component {
       <div>
  <div class="flex-container">
   <div><img src={images.mcDonalds} alt="McDonalds"></img></div>
-  <div>2</div>
-  <div>3</div>
-  <div>4</div>
-  <div>5</div>
-  <div>6</div>
-  <div>7</div>
-  <div>8</div>
-  <div>9</div>
-  <div>10</div>
-  <div>11</div>
-  <div>12</div>
+  <div><img src={images.blaze} alt="Blaze"></img></div>
+  <div><img src={images.chik} alt="Chik-Fil-A"></img></div>
+  <div><img src={images.chipotle} alt="Chipotle"></img></div>
+  <div><img src={images.fiveGuys} alt="Five Guys"></img></div>
+  <div><img src={images.freshii} alt="Freshii"></img></div>
+  <div><img src={images.greekKitchen} alt="Greek Kitchen"></img></div>
+  <div><img src={images.panera} alt="Panera Bread"></img></div>
+  <div><img src={images.penn} alt="Penn Station"></img></div>
+  <div><img src={images.popeyes} alt="Popeyes"></img></div>
+  <div><img src={images.portillos} alt="Portillos"></img></div>
+  <div><img src={images.wendys} alt="Wendys"></img></div>
 </div> 
       </div>
     )
